89e997a291608616e0e118e27ca52e966a3eb570
Rjoydip <joydipand@gmail.com>
2017-09-17 10:33:51 -0700

    commit 3

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.post('/quotes', jsonParser, (req, res) => {
   });
 });
 
-// app.listen(app.get('port'), function () {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
