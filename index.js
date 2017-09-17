cfc548c95a1e942750721fbdc96c67b896b5de7b
Rjoydip <joydipand@gmail.com>
2017-09-17 08:26:58 -0700

    route all

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ MongoClient.connect(mongo_url, function (err, _db) {
 
 app.set('port', (process.env.PORT || 8000));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   db.collection('quotes').save({
     "_id": Math.random(),
     "name": "quotes one"
