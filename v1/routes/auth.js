a57f35c6f9653b37bc98a386735b4f6f7c3916f2
Rjoydip <joydipand@gmail.com>
2017-09-19 12:49:06 -0700

    chore(): others api endpoint added

--- v1/routes/auth.js
+++ v1/routes/auth.js
@@ -11,4 +11,44 @@ routes.get('/', (req, res) => {
     });
 });
 
+routes.get('/users', (req, res) => {
+    res.send({
+        status: 200,
+        data: {
+            users: [
+                {
+                    name: "user one",
+                    country: "India"
+                },
+                {
+                    name: "user two",
+                    country: "Pakistan"
+                },
+                {
+                    name: "user three",
+                    country: "bangladesh"
+                }
+            ]
+        }
+    });
+});
+
+routes.post('/login', (req, res) => {
+    res.send({
+        status: 200,
+        data: {
+            message: "Login successfull"
+        }
+    });
+});
+
+routes.post('/registration', (req, res) => {
+    res.send({
+        status: 200,
+        data: {
+            message: "Registration successfull"
+        }
+    });
+});
+
 module.exports = routes;
